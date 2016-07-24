85c87f13d7ba9a48327343c05b4ef4647b5b3939
Piyush Santwani <psantwani@gmail.com>
2016-07-24 21:21:03 +0530

    localhost:3000

--- public/assets/javascripts/api.js
+++ public/assets/javascripts/api.js
@@ -6,7 +6,7 @@ function load_data(){
   $.ajax({
         type: "GET",
         dataType: "html",
-        url:'/trending_places/list',
+        url:'http://localhost:300/trending_places/list',
         success: function(data){                    
             $('#favorite_places').html($(data).filter('#trending_places').html());
             stack_card();
@@ -16,7 +16,7 @@ function load_data(){
    $.ajax({
         type: "GET",
         dataType: "json",
-        url:'/city_airports/list',
+        url:'http://localhost:3000/city_airports/list',
         success: function(data){                    
             city_airports_list = data;          
             $.each(data, function() {
