8aee4ec4a39a26c84b5cf8be6c283271a9b511af
Piyush Santwani <psantwani@gmail.com>
2016-07-24 21:23:35 +0530

    http to https

--- public/assets/javascripts/api.js
+++ public/assets/javascripts/api.js
@@ -6,7 +6,7 @@ function load_data(){
   $.ajax({
         type: "GET",
         dataType: "html",
-        url:'http://localhost:300/trending_places/list',
+        url:'https://localhost:300/trending_places/list',
         success: function(data){                    
             $('#favorite_places').html($(data).filter('#trending_places').html());
             stack_card();
@@ -16,7 +16,7 @@ function load_data(){
    $.ajax({
         type: "GET",
         dataType: "json",
-        url:'http://localhost:3000/city_airports/list',
+        url:'https://localhost:3000/city_airports/list',
         success: function(data){                    
             city_airports_list = data;          
             $.each(data, function() {
